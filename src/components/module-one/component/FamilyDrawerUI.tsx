4ae598a6d2005ba65d967a8c8aae0f9f20da8771
Shubham Yelekar <shubhamyelekar0@gmail.com>
2026-04-26 14:55:00 +0530

    Family Drawer start

--- src/components/module-one/component/FamilyDrawerUI.tsx
+++ src/components/module-one/component/FamilyDrawerUI.tsx
@@ -1,11 +1,21 @@
-import React from "react";
-
+import { useState } from "react";
+import { Drawer } from "vaul";
 const FamilyDrawer = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <button className="cursor-pointer rounded-full border border-neutral-200 bg-white px-4 py-2 text-sm font-semibold text-neutral-700 transition-[color,transform] duration-300 hover:bg-neutral-50 active:scale-[0.97]">
+      <button
+        onClick={() => setIsOpen(true)}
+        className="cursor-pointer rounded-full border border-neutral-200 bg-white px-4 py-2 text-sm font-semibold text-neutral-700 transition-[color,transform] duration-300 hover:bg-neutral-50 active:scale-[0.97]"
+      >
         Open Drawer
       </button>
+      <Drawer.Root open={isOpen} onOpenChange={setIsOpen}>
+        <Drawer.Portal>
+          <Drawer.Overlay className="fixed inset-0 z-10 bg-black/20" />
+          <Drawer.Content className="fixed inset-x-4 bottom-4 z-10 mx-auto h-64 max-w-90 overflow-hidden rounded-[36px] bg-[#FEFFFE] outline-hidden md:mx-auto md:w-full"></Drawer.Content>
+        </Drawer.Portal>
+      </Drawer.Root>
     </div>
   );
 };
