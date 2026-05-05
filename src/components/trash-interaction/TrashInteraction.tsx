e243c2a647aaa795f54d6b69ba11a24353e969d6
Shubham Yelekar <shubhamyelekar0@gmail.com>
2026-05-05 15:52:13 +0530

    trash deleting animation

--- src/components/trash-interaction/TrashInteraction.tsx
+++ src/components/trash-interaction/TrashInteraction.tsx
@@ -5,10 +5,10 @@ const TrashInteraction = () => {
   return (
     <section>
       <div className="px-2 py-4">
-        <h4 className="text-md font-semibold">Multi step component</h4>
+        <h4 className="text-md font-semibold">Trash component</h4>
 
         <p className="text-sm text-neutral-400 dark:text-neutral-500">
-          Multi step form component with animation that moves forward and back.
+          A deleting with a style.
         </p>
       </div>
       <ComponentBlock>
