61f3cc51baf5e8de374bcdfb59e3092b78429afe
Shubham Yelekar <shubhamyelekar0@gmail.com>
2026-05-29 12:18:28 +0530

    Update Ring.tsx

--- src/components/dynamic-island/Ring.tsx
+++ src/components/dynamic-island/Ring.tsx
@@ -33,23 +33,54 @@ export function Ring() {
           />
         ) : null}
       </AnimatePresence>
-      <motion.div className="relative h-[12.75px] w-[11.25px]" animate={{ rotate: isSilent ? [0, -15, 5, -2, 0] : [0, 20 , -15, 12.5, -10 , 10, -7.5, -5, 5, 0] , x: isSilent ? 9 : 0}}>
+      <motion.div
+        className="relative h-[12.75px] w-[11.25px]"
+        animate={{
+          rotate: isSilent
+            ? [0, -15, 5, -2, 0]
+            : [0, 20, -15, 12.5, -10, 10, -7.5, -5, 5, 0],
+          x: isSilent ? 9 : 0,
+        }}
+      >
         <svg
-          className="absolute inset-0"
-          width="11.25"
-          height="12.75"
-          viewBox="0 0 15 17"
-          fill="none"
           xmlns="http://www.w3.org/2000/svg"
+          width="12"
+          height="13"
+          viewBox="0 0 12 13"
+          fill="none"
         >
-          <path
-            d="M1.17969 13.3125H13.5625C14.2969 13.3125 14.7422 12.9375 14.7422 12.3672C14.7422 11.5859 13.9453 10.8828 13.2734 10.1875C12.7578 9.64844 12.6172 8.53906 12.5547 7.64062C12.5 4.64062 11.7031 2.57812 9.625 1.82812C9.32812 0.804688 8.52344 0 7.36719 0C6.21875 0 5.40625 0.804688 5.11719 1.82812C3.03906 2.57812 2.24219 4.64062 2.1875 7.64062C2.125 8.53906 1.98438 9.64844 1.46875 10.1875C0.789062 10.8828 0 11.5859 0 12.3672C0 12.9375 0.4375 13.3125 1.17969 13.3125ZM7.36719 16.4453C8.69531 16.4453 9.66406 15.4766 9.76562 14.3828H4.97656C5.07812 15.4766 6.04688 16.4453 7.36719 16.4453Z"
-            fill="white"
-          />
+          <g clip-path="url(#clip0_331_414)">
+            <motion.path
+              d="M5.52539 12.334C6.52148 12.334 7.24805 11.6075 7.32422 10.7871H3.73242C3.80859 11.6075 4.53516 12.334 5.52539 12.334Z"
+              fill="green"
+                      animate={{
+          rotate: isSilent
+            ? [0, 15, -5, 2, 0]
+            : [0,15, -15,0],
+        }}
+            />
+            <path
+              d="M0.884768 9.98438H10.1719C10.7227 9.98438 11.0567 9.70313 11.0567 9.2754C11.0567 8.68943 10.459 8.1621 9.95505 7.64062C9.56835 7.23633 9.4629 6.40429 9.41603 5.73046C9.375 3.48046 8.77732 1.93359 7.21875 1.37109C6.99609 0.603516 6.39258 0 5.52539 0C4.66406 0 4.05469 0.603516 3.83789 1.37109C2.27929 1.93359 1.68164 3.48046 1.64062 5.73046C1.59375 6.40429 1.48829 7.23633 1.10156 7.64062C0.591797 8.1621 0 8.68943 0 9.2754C0 9.70313 0.328125 9.98438 0.884768 9.98438Z"
+              fill="white"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_331_414">
+              <rect width="11.25" height="12.75" fill="white" />
+            </clipPath>
+          </defs>
         </svg>
         {isSilent ? (
           <div className="absolute inset-0 h-5 translate-x-[5px] -translate-y-[5px] rotate-[-40deg]">
-            <motion.div animate={{height : isSilent? 16: 0}} transition={{ease : "easeInOut", duration: isSilent ? 0.125: 0.05, delay : isSilent? 0.15: 0}}  className="w-fit rounded-full">
+            <motion.div
+              animate={{ height: isSilent ? 16 : 0 }}
+              transition={{
+                ease: "easeInOut",
+                duration: isSilent ? 0.125 : 0.05,
+                delay: isSilent ? 0.15 : 0,
+              }}
+              className="w-fit rounded-full"
+            >
               <div className="flex h-full w-[3px] items-center justify-center rounded-full bg-[#FD4F30]">
                 <div className="h-full w-[0.75px] rounded-full bg-white" />
               </div>
